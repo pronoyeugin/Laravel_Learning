892bbbc91789fa663972386b884f2b9ebcb639bc
unknown <pronoykhalko@gmail.com>
2026-09-25 00:15:09 +0600

    Initial Laravel application

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page 
+    </x-slot:heading>
+    <h1>This is contact page !</h1>
+
+</x-layout>
+
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
+
